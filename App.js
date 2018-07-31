45602d650af21478a6b5bbdd9ba348f824f7be6d
Elodie Royant <eroyant@gmail.com>
2018-08-01 00:27:45 +0200

    simple login with facebook connect

--- App.js
+++ App.js
@@ -1,73 +1,70 @@
-import React from 'react'
-import { 
-  StyleSheet,
-  Platform,
-  SafeAreaView,
-  View,
-} from 'react-native'
-import { compose, withHandlers, withProps } from 'recompose'
+import React, { Component } from 'react'
+import { Image, Button, Text, View } from 'react-native'
+import { AuthSession } from 'expo'
 
-import { getSpots, handleGetDirections } from './utils/localize'
-import importFont from './utils/importFont'
-import notify from './utils/notify'
-import { emitSelectSpot } from './utils/sockets'
+const FB_APP_ID = '261733521288349'
 
-import Map from './components/Map'
-import Layout from './components/Layout'
-import ArrivalModal from './components/ArrivalModal'
+export default class App extends Component {
+  state = {
+    userInfo: null,
+  };
 
-
-const AppContainer = (props) => {
-  const { fontLoaded } = props
-  let display
-  if (Platform.OS === 'ios') {
-    display = (
-        <SafeAreaView style={styles.container}>
-          <ArrivalModal {...props} />
-          <Map {...props} />
-        </SafeAreaView>
-    )
-  } else {
-    display = (
-        <View style={styles.container}>
-          <ArrivalModal {...props} />
-          <Map {...props} />
-        </View>
-    )
-  } 
-  if (fontLoaded) {
-    display = (
-      <Layout>
-        {display}
-      </Layout>
-    )
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {!this.state.userInfo ? (
+          <Button title="Open FB Auth" onPress={this._handlePressAsync} />
+        ) : (
+          this._renderUserInfo()
+        )}
+      </View>
+    );
   }
-  return ( display )
-}
 
-const enhance = compose(
-  getSpots,
-  importFont,
-  notify,
-  withHandlers({ 
-    handleOnPress: props => e => {
-      props.registerForPushNotifications()
-      emitSelectSpot(e)
-      e.persist()
-      props.watchPositionAsync()
-      handleGetDirections(e)
+  _renderUserInfo = () => {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <Image
+          source={{ uri: this.state.userInfo.picture.data.url }}
+          style={{ width: 100, height: 100, borderRadius: 50 }}
+        />
+        <Text style={{ fontSize: 20 }}>{this.state.userInfo.name}</Text>
+        <Text>ID: {this.state.userInfo.id}</Text>
+      </View>
+    );
+  };
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+
+    // You need to add this url to your authorized redirect urls on your Facebook app
+    console.log({ redirectUrl });
+
+    // NOTICE: Please do not actually request the token on the client (see:
+    // response_type=token in the authUrl), it is not secure. Request a code
+    // instead, and use this flow:
+    // https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow/#confirm
+    // The code here is simplified for the sake of demonstration. If you are
+    // just prototyping then you don't need to concern yourself with this and
+    // can copy this example, but be aware that this is not safe in production.
+
+    let result = await AuthSession.startAsync({
+      authUrl:
+        `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
+        `&client_id=${FB_APP_ID}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    });
+
+    if (result.type !== 'success') {
+      alert('Uh oh, something went wrong');
+      return;
     }
-  }),
-)
 
-const App = enhance(AppContainer)
-
-export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-})
+    let accessToken = result.params.access_token;
+    let userInfoResponse = await fetch(
+      `https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,picture.type(large)`
+    );
+    const userInfo = await userInfoResponse.json();
+    this.setState({ userInfo });
+  };
+}
