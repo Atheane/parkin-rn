67e2581415aaa66279a6f8054eb38c4c37b4ab7a
Elodie Royant <eroyant@gmail.com>
2018-12-02 11:49:15 +0100

    proper initial state

--- reducers/index.js
+++ reducers/index.js
@@ -1,21 +1,23 @@
 import * as types from '../constants/ActionTypes'
 
-export default (currentState, action) => {
-  if (currentState === undefined) {
-    return {}
-  } else {
-    switch (action.type) {
-      case types.SET_CONNECTION:
-        action.payload
-        break
-      case types.SET_POSITION:
-        action.payload
-        break
-      case types.GET_SPOTS:
-        action.payload
-        break
-      default:
-        currentState
-    }
+const INITIAL_STATE = {
+  socketId: '',
+  userPosition: {},
+  spots: []
+};
+
+export default (currentState = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case types.SET_CONNECTION:
+      action.payload
+      break
+    case types.SET_POSITION:
+      action.payload
+      break
+    case types.GET_SPOTS:
+      action.payload
+      break
+    default:
+      currentState
   }
 }
