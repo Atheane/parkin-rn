3806eaf63cea1aa6aceb232ac8daf15afe654228
Elodie Royant <eroyant@gmail.com>
2018-12-11 14:56:02 +0100

    spot button design, to-do: drop-shadow

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { MapView, Svg } from 'expo'
+import { MapView } from 'expo'
 import { PROVIDER_GOOGLE } from 'react-native-maps'
-import { View, TouchableHighlight, Image } from 'react-native'
+import { View } from 'react-native'
 import NightStyle from '../constants/NightStyle'
 import MarkerList from '../containers/MarkerList'
+import SpotButton from './SpotButton.js'
 
 
 export default (props) => {
@@ -22,31 +23,7 @@ export default (props) => {
         >
           <MarkerList spots={spots}/>
         </MapView>
-        <TouchableHighlight onPress={console.log('Should give a spot')} style={styles.button}>
-          <Svg height="100" width="100" fill="none" style={styles.svg} >
-            <Svg.Defs>
-              <Svg.LinearGradient
-                id="grad"
-                gradientUnits="userSpaceOnUse"
-                x1="0"
-                y1="0"
-                x2="0"
-                y2="50"
-            >          
-                <Svg.Stop offset="0" stopColor="red" stopOpacity="0" />
-                <Svg.Stop offset="1" stopColor="blue" stopOpacity="1" />
-              </Svg.LinearGradient>
-            </Svg.Defs>
-            <Svg.Circle
-              cx="30"
-              cy="30"
-              r="27"
-              strokeWidth={2}
-              stroke="rgb(255,255,255)"
-              fill="url(#grad)"
-            />
-          </Svg>
-        </TouchableHighlight>
+        <SpotButton />
       </View>
   )
 }
@@ -60,19 +37,4 @@ const styles = {
     width: 32,
     height: 32,
   },
-  button: {
-    position: 'absolute',
-    width: 70, 
-    height: 70, 
-    bottom: 30, 
-    right: 30,
-    zIndex: 1,
-    // backgroundColor: "transparent"
-  },
-  svg: {
-    position: 'relative',
-    bottom: 0, 
-    right: 0, 
-    zIndex: 2
-  }
 }
---
+++ components/SpotButton.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Svg } from 'expo'
+import { TouchableHighlight } from 'react-native'
+
+export default (props) => {
+
+  return (
+    <TouchableHighlight onPress={console.log('Should give a spot')} style={styles.button} >
+      <Svg height="100" width="100" fill="rgb(238,162,35)" style={styles.svg} >
+        <Svg.Defs>
+          <Svg.LinearGradient
+            id="grad"
+            gradientUnits="userSpaceOnUse"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="50"
+          >          
+            <Svg.Stop offset="0" stopColor="rgb(238,162,35)" />
+            <Svg.Stop offset="1" stopColor="rgb(254,118,82)" stopOpacity="1" />
+          </Svg.LinearGradient>
+        </Svg.Defs>
+        <Svg.Circle
+          cx="40"
+          cy="40"
+          r="36"
+          strokeWidth={3}
+          stroke="rgb(255,255,255)"
+          fill="url(#grad)"
+        />
+        <Svg.Circle
+          cx="40"
+          cy="40"
+          r="15"
+          strokeWidth={3}
+          stroke="rgb(255,255,255)"
+          fill="rgb(255,214,157)"
+        />
+        <Svg.Polygon             
+            points="34,54 46,54 40,62"
+            fill="rgb(255,255,255)"
+            stroke="rgb(255,255,255)"
+            strokeWidth={1} /> 
+        </Svg>
+    </TouchableHighlight>
+  )
+}
+
+
+
+
+const styles = {
+  button: {
+    position: 'absolute',
+    width: 70, 
+    height: 70, 
+    bottom: 30, 
+    right: 30,
+    zIndex: 1,
+    // backgroundColor: "transparent"
+  },
+  svg: {
+    position: 'relative',
+    bottom: 0, 
+    right: 0, 
+    zIndex: 2
+  }
+}
